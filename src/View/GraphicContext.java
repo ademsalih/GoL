16bfafae27dd14819549edb5b130624b1a92bbb4
Patrik Kvarme Hansen <s3103973@stud.hioa.no>
2017-02-09 13:52:38 +0100

    Created GraphicContext-class and populated with basic functions.

---
+++ src/View/GraphicContext.java
@@ -0,0 +1,27 @@
+package View;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+/**
+ * Created by patrikkvarmehansen on 09/02/17.
+ *
+ * This class draws to the canvas.
+ */
+public class GraphicContext {
+
+    //Method that sets the color for the cells.
+    public void setColor () {
+    }
+
+    //Takes cell position and draws a square in a given size.
+    public void drawCell (GraphicsContext gc, int x, int y, int size) {
+        gc.fillRect(x, y, size, size);
+    }
+
+
+
+
+
+
+}
