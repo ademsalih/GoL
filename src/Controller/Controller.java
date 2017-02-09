b411f51361c8e2ddf81be933ca7e3a1e847f3243
Narmathasiva <narmatha.fl@gmail.com>
2017-02-09 14:45:10 +0100

    Controller mappen

---
+++ src/Controller/Controller.java
@@ -0,0 +1,42 @@
+package Controller;
+
+/**
+ * Created by Narmatha on 09.02.2017.
+ */
+public class Controller {
+    private boolean isStarted;
+
+    protected void startStopBtnClicked(){
+
+    }
+
+    protected void stop(){
+
+    }
+
+    protected void start(){
+
+    }
+
+    protected void nextBtnClicked(){
+
+    }
+
+    protected void speedChange(){
+
+    }
+
+    protected void zoomChange(){
+
+    }
+
+    protected void Shapeselected(String a){
+
+    }
+
+    protected void clearBoard(){
+
+    }
+
+
+}
