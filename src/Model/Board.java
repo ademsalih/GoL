4faf4813a6ecf183fd44fcc1a691dc7ae89259b2
Patrik Kvarme Hansen <s3103973@stud.hioa.no>
2017-02-13 11:52:57 +0100

    Removed GolData.java and refactored all data into Board.java

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -7,9 +7,34 @@ package Model;
  */
 public class Board {
     private byte[][] board;
+    private int cellSize;
+    private int columns;
+    private int rows;
+    private String cellColor;
 
-    //Creates a board object that has a 2D array that corresponds to the number of rows and columns.
-    public Board (int columns, int rows) {
+    public Board (int cellSize, int columns, int rows, String cellColor) {
+        this.cellSize = cellSize;
+        this.rows = rows;
+        this.columns = columns;
+        this.cellColor = cellColor;
         this.board = new byte[columns][rows];
     }
+
+    public int getCellSize() {
+        return cellSize;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public String getCellColor() {
+        return cellColor;
+    }
+
+
 }
