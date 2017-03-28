625b2d43380831ab596c641bbae450a698aaa22b
Narmathasiva <narmatha.fl@gmail.com>
2017-03-28 17:51:04 +0200

    Divided TestingRule into "flere" methods

--- src/Test/TestingRule.java
+++ src/Test/TestingRule.java
@@ -18,11 +18,26 @@ public class TestingRule {
                 {0, 0, 1}
         };
 
+        Rule rule = new Rule(board);
+        org.junit.Assert.assertEquals(rule.toString(),"100010001");
+
+    }
+
+    @Test
+    public void testToString1(){
+
         byte[][] board1 = {
                 {0,1,0},
                 {1,1,1},
                 {0,1,0}
         };
+
+        Rule rule1 = new Rule(board1);
+        org.junit.Assert.assertEquals(rule1.toString(),"010111010");
+    }
+
+    @Test
+    public void testToString2() {
 
         byte[][] board2 = {
                 {0,0,1,0,0},
@@ -31,12 +46,6 @@ public class TestingRule {
                 {0,1,1,1,0},
                 {0,0,1,0,0}
         };
-
-        Rule rule = new Rule(board);
-        org.junit.Assert.assertEquals(rule.toString(),"100010001");
-
-        Rule rule1 = new Rule(board1);
-        org.junit.Assert.assertEquals(rule1.toString(),"010111010");
 
         Rule rule2 = new Rule(board2);
         org.junit.Assert.assertEquals(rule2.toString(),"0010001110110110111000100");
@@ -51,11 +60,28 @@ public class TestingRule {
                 { 0, 0, 0, 0 }
         };
 
+        Rule rule = new Rule(board);
+        rule.setCurrentBoard(rule.conwaysBoardRules());
+        org.junit.Assert.assertEquals(rule.toString(),"0000011001100000");
+
+    }
+
+    @Test
+    public void testNextGeneration1(){
+
         byte[][] board1 = {
                 {0,1,0},
                 {1,1,1},
                 {0,1,0}
         };
+
+        Rule rule1 = new Rule(board1);
+        rule1.setCurrentBoard(rule1.conwaysBoardRules());
+        org.junit.Assert.assertEquals(rule1.toString(),"111101111");
+    }
+
+    @Test
+    public void testNextGeneration2(){
 
         byte[][] board2 = {
                 {0,0,1,0,0},
@@ -65,19 +91,9 @@ public class TestingRule {
                 {0,0,1,0,0}
         };
 
-
-        Rule rule = new Rule(board);
-        rule.setCurrentBoard(rule.conwaysBoardRules());
-        org.junit.Assert.assertEquals(rule.toString(),"0000011001100000");
-
-        Rule rule1 = new Rule(board1);
-        rule1.setCurrentBoard(rule1.conwaysBoardRules());
-        org.junit.Assert.assertEquals(rule1.toString(),"111101111");
-
         Rule rule2 = new Rule(board2);
         rule2.setCurrentBoard(rule2.conwaysBoardRules());
         org.junit.Assert.assertEquals(rule2.toString(),"0111010001100011000101110");
-
     }
 
     @Test
@@ -102,11 +118,29 @@ public class TestingRule {
                 {0, 0, 1}
         };
 
+        Rule rule = new Rule(board);
+        int c = rule.countNeighbor(board, 1, 1);
+        org.junit.Assert.assertEquals(c,2);
+
+    }
+
+    @Test
+    public void testCountNeighbor1(){
+
         byte[][] board1 = {
                 {0,1,0},
                 {1,1,1},
                 {0,1,0}
         };
+
+        Rule rule1 = new Rule(board1);
+        int c1 = rule1.countNeighbor(board1, 0, 0);
+        org.junit.Assert.assertEquals(c1,3);
+
+    }
+
+    @Test
+    public void testCountNeighbor2() {
 
         byte[][] board2 = {
                 {0,0,1,0,0},
@@ -116,18 +150,9 @@ public class TestingRule {
                 {0,0,1,0,0}
         };
 
-        Rule rule = new Rule(board);
-        int c = rule.countNeighbor(board, 1, 1);
-        org.junit.Assert.assertEquals(c,2);
-
-        Rule rule1 = new Rule(board1);
-        int c1 = rule1.countNeighbor(board1, 0, 0);
-        org.junit.Assert.assertEquals(c1,3);
-
         Rule rule2 = new Rule(board2);
         int c2 = rule2.countNeighbor(board2, 2, 2);
         org.junit.Assert.assertEquals(c2,8);
 
     }
-
 }
