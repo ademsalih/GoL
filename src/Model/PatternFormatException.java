dfa868ef7f32604c741ce7b5f64f9822722d1382
Narmathasiva <narmatha.fl@gmail.com>
2017-05-05 13:12:34 +0200

    fixed most fo the warnings on PatternFormatException

--- src/Model/PatternFormatException.java
+++ src/Model/PatternFormatException.java
@@ -1,10 +1,8 @@
 package Model;
 
-/**
- * Created by patrikkvarmehansen on 03/04/17.
- */
+
 public class PatternFormatException extends Exception {
-    public String error;
+    private String error;
 
     public PatternFormatException(String error) {
         this.error = "Error reading board from .rle-file" + error;
