52467e74dd70207d0bb9b352e0b14e3f9cbf07e4
Narmathasiva <narmatha.fl@gmail.com>
2017-05-05 16:30:21 +0200

    did some javadoccc on point

--- src/Model/Point.java
+++ src/Model/Point.java
@@ -4,7 +4,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 /**
- * This class draws rectangles and fills them with color on a point.
+ * This class draws rectangles and fills them with a color on a point.
  */
 
 public class Point {
@@ -21,6 +21,12 @@ public class Point {
         this.y = y;
     }
 
+    /**
+     * Draws rectangles on a given points and fills them with a specified color
+     * @param gc
+     * @param drawColor
+     * @param cellSize
+     */
     public void draw(GraphicsContext gc, Color drawColor, double cellSize) {
         gc.setFill(drawColor);
         gc.fillRect(xOffset + x, yOffset + y, xOffset + cellSize, yOffset + cellSize);
